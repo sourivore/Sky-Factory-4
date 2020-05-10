660c8e013bae1ad7bfd459886f1a419219d372fe
David Hébert <pro.david.hebert@gmail.com>
2020-05-11 01:13:34 +0200

    Change prepdisk

--- Master/prepdisk.lua
+++ Master/prepdisk.lua
@@ -3,7 +3,7 @@ local f = assert (io.popen ("ls /mnt"))
 for line in f:lines() do
   if line ~= "8ef" then
 	os.execute("mkdir /mnt/"..line.."/bin /mnt/"..line.."/usr/lib /mnt/"..line.."/home/programs")
-    os.execute("cp /bin/repaste.lua /mnt/"..line.."/bin/repaste.lua")
+    os.execute("cp /home/programs/getsources.lua /bin/getsources.lua /mnt/"..line.."/bin/getsources.lua")
     os.execute("cp /usr/lib/_config.lua /mnt/"..line.."/usr/lib/_config.lua")
   end
 end
