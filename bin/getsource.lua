cd2dfe492f2b87c11db30304f90c6521b67223e0
David Hébert <pro.david.hebert@gmail.com>
2020-05-10 22:48:05 +0200

    Change file path

--- bin/getsource.lua
+++ bin/getsource.lua
@@ -3,7 +3,7 @@ local internet = require("internet")
 local rootPath = config.rootPath
 
 for fileName, destPath in pairs(config.folders) do
-    local files = internet.request(rootPath..id..".txt")
+    local files = internet.request(rootPath..fileName..".txt")
     for file in files do
         print("LINE", file)
     end
