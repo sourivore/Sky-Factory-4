4c903b720ad102f78299a6a5f87085a80da39e30
David Hébert <pro.david.hebert@gmail.com>
2020-05-11 00:38:01 +0200

    Force push

--- bin/getsources.lua
+++ bin/getsources.lua
@@ -5,8 +5,8 @@ local rootPath = config.rootPath
 for fileName, destPath in pairs(config.folders) do
     local files = internet.request(rootPath.."files/"..fileName..".txt")
     for file in files do
-        for line in string.gmatch(file, "[_-.%w]+") do
-            os.execute("wget "..rootPath..fileName.."/"..line.." "..destPath.."/"..line)
+        for line in string.gmatch(file, "[%w_-.]+") do
+            os.execute("wget -f "..rootPath..fileName.."/"..line.." "..destPath.."/"..line)
         end 
     end
 end
