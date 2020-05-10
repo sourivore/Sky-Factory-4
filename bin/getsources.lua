ed5d56218b811acf0ccd3281c8088dce4c87d797
David Hébert <pro.david.hebert@gmail.com>
2020-05-11 00:47:29 +0200

    Fix line pattern

--- bin/getsources.lua
+++ bin/getsources.lua
@@ -5,7 +5,7 @@ local rootPath = config.rootPath
 for fileName, destPath in pairs(config.folders) do
     local files = internet.request(rootPath.."files/"..fileName..".txt")
     for file in files do
-        for line in string.gmatch(file, "[%w_-.]+") do
+        for line in string.gmatch(file, "[%w-._]+") do
             os.execute("wget -f "..rootPath..fileName.."/"..line.." "..destPath.."/"..line)
         end 
     end
