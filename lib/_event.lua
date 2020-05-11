ebc70e4fd9b2a4cf84eddcb81d998e04213e126b
David Hébert <pro.david.hebert@gmail.com>
2020-05-11 11:12:11 +0200

    Reverse _event

--- lib/_event.lua
+++ lib/_event.lua
@@ -3,8 +3,8 @@ local event = require("event")
 local _event = {}
 
 _event.removeListeners = function(key)
-	for k,v in pairs(event.handlers) do
-		if k == key then
+	for _,v in pairs(event.handlers) do
+		if v.key == key then
 			event.ignore(v.key, v.callback)
 		end
 	end
