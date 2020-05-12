f9d58b3420016d003edd9acbec8eaa973f0be021
David Hébert <pro.david.hebert@gmail.com>
2020-05-12 18:46:44 +0200

    Add case functions

--- lib/_logic.lua
+++ lib/_logic.lua
@@ -5,11 +5,19 @@ local _logic = {}
 local test
 
 _logic.switch = function(...)
-  test = {...}
+	test = {...}
 end
 
 _logic.case = function(...)
-  return _table.equals(test, {...})
+	return _table.equals(test, {...})
+end
+
+_logic.caseCheck = function(testFunction, ...)
+	return _table.check(testFunction, test, {...})
+end
+
+_logic.caseFilter = function(filterFunction, ...)
+	return _table.filter(filterFunction, test, {...})
 end
 
 return _logic
--- lib/_table.lua
+++ lib/_table.lua
@@ -1,6 +1,6 @@
 local _table = {}
 
-_table.equals = function(table1, table2)
+local equals = function(table1, table2)
 	if #table1 ~= #table2 then
 		return false
 	end
@@ -10,6 +10,30 @@ _table.equals = function(table1, table2)
 		end
 	end
 	return true
+end
+
+_table.equals = equals
+
+_table.check = function(checkFunction, tableTest, tableCheck)
+	return checkFunction(tableTest, tableCheck)
+end
+
+_table.filter = function(filterFunction, tableTest, tablesCheck)
+	local result = {}
+	for _, tableCheck in pairs(tablesCheck) do
+		if filterFunction(tableTest, tableCheck) then
+			result.insert(tableCheck)
+		end
+	end
+	return result
+end
+
+_table.remove = function(tableValues, value)
+	for index, tableValue in pairs(tableValues) do
+		if equals(tableValue, value) then
+			table.remove(tableValues, index)
+		end
+	end
 end
 
 return _table
