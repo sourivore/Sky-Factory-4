d88e18163cddd06dc5a189fd518a300d85123edf
David Hébert <pro.david.hebert@gmail.com>
2020-05-14 16:04:26 +0200

    Add _table.count

--- lib/_table.lua
+++ lib/_table.lua
@@ -63,4 +63,12 @@ _table.contains = function(tableValues, value)
 	return false
 end
 
+_table.length = function (t)
+    local count = 0
+    for _, _ in pairs(t) do
+        count = count + 1
+    end
+    return count
+end
+
 return _table
